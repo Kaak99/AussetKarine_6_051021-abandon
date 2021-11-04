ee4bb012afdaeaeb2e548f1e7ec584bc4b919ecf
Kaak <cours.kaak@gmail.com>
2021-11-04 16:43:09 +0100

    modif server.js

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,59 @@
 // tests (à retirer)
-console.log(` --------> tests `);
-console.log(` tests `);
+console.log("**********start**********");
+//console.log(` tests `);
 
 // packages import
 
 const http = require("http");
-//console.log(http);
 const app = require("./app.js");
-//console.log(app);
+
 const dotenv = require("dotenv");
 //console.log(dotenv);
 //const result = dotenv.config();
+//ou : const dotenv = require("dotenv").config();
 
 
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
-// param du port
-//app.set("port", 3000);
-app.set("port", process.env.PORT);
-
-//create server
 const server = http.createServer(app);
 
-//écoute le port 3000
-server.listen(3000);
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
 
-
-
-
-
+server.listen(port);
